d0d82c2551790d829db4a483c685a637803cb916
xiaokai <xiaokai@kk.com>
2023-01-04 20:49:16 +0700

    更改播放模式

--- app/src/main/java/com/example/videoplay/MainActivity.java
+++ app/src/main/java/com/example/videoplay/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.videoplay;
 
+import android.app.Application;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-
-import androidx.navigation.ui.AppBarConfiguration;
 
 import com.example.videoplay.databinding.ActivityMainBinding;
 import com.hjq.permissions.OnPermissionCallback;
@@ -19,18 +24,54 @@ import com.zlylib.fileselectorlib.FileSelector;
 import com.zlylib.fileselectorlib.bean.EssFile;
 import com.zlylib.fileselectorlib.utils.Const;
 
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import tcking.github.com.giraffeplayer2.GiraffePlayer;
 import tcking.github.com.giraffeplayer2.Option;
+import tcking.github.com.giraffeplayer2.PlayerListener;
+import tcking.github.com.giraffeplayer2.PlayerManager;
 import tcking.github.com.giraffeplayer2.VideoInfo;
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+import tv.danmaku.ijk.media.player.IjkTimedText;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+
+    String defKey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+    //获取剪切板内容
+    public String getClipContent() {
+        ClipboardManager manager = (ClipboardManager) getApplication().getSystemService(Context.CLIPBOARD_SERVICE);
+        if (manager != null) {
+            if (manager.hasPrimaryClip() && manager.getPrimaryClip().getItemCount() > 0) {
+                CharSequence addedText = manager.getPrimaryClip().getItemAt(0).getText();
+                String addedTextString = String.valueOf(addedText);
+                if (!TextUtils.isEmpty(addedTextString)) {
+                    return addedTextString;
+                }
+            }
+        }
+        return "";
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation != Configuration.ORIENTATION_LANDSCAPE) {
+
+        }
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//隐藏软键盘 //
+        imm.hideSoftInputFromWindow(binding.textInputLayout.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(binding.textInputLayoutKey.getWindowToken(), 0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +83,9 @@ public class MainActivity extends AppCompatActivity {
         binding.btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!binding.textInputLayout.getText().toString().equals("")) {
-                    VideoInfo videoInfo = new VideoInfo(binding.textInputLayout.getText().toString());
+                String key = binding.textInputLayout.getText().toString();
+                if (!key.equals("")) {
+                    VideoInfo videoInfo = new VideoInfo(key);
                     play(videoInfo);
                 } else {
                     Toast.makeText(MainActivity.this, "请输入播放地址在播放", Toast.LENGTH_SHORT).show();
@@ -62,6 +104,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         getPermissions();
+
+//        binding.videoView.s
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        binding.textInputLayoutKey.setText(getKey());
+        String url = getClipContent();
+        if (!TextUtils.isEmpty(url)) {
+            binding.textInputLayout.setText(url);
+            binding.btnPlay.callOnClick();
+        }
+    }
+
+    void saveKey(String key) {
+        //步骤2-1：创建一个SharedPreferences.Editor接口对象，lock表示要写入的XML文件名，MODE_WORLD_WRITEABLE写操作
+        SharedPreferences.Editor editor = getSharedPreferences("key", MODE_PRIVATE).edit();
+        //步骤2-2：将获取过来的值放入文件
+        editor.putString("key", key);
+        //步骤3：提交
+        editor.commit();
+    }
+
+    String getKey() {
+        //步骤1：创建一个SharedPreferences接口对象
+        SharedPreferences read = getSharedPreferences("key", MODE_PRIVATE);
+        //步骤2：获取文件中的值
+        String value = read.getString("key", defKey);
+        return value;
     }
 
     // 获取权限
@@ -100,8 +172,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void play(VideoInfo videoInfo) {
-        videoInfo.addOption(Option.create(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "decryption_key", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
-        GiraffePlayer.play(getBaseContext(), videoInfo);
+        JSONObject jsonObject = new JSONObject();
+        String key = binding.textInputLayoutKey.getText().toString();
+        if (!TextUtils.isEmpty(key)) {
+            if (key.length() == 32) {
+                videoInfo.addOption(Option.create(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "decryption_key", key));
+                saveKey(key);
+            } else {
+                Toast.makeText(MainActivity.this, "密钥长度不对 未使用解密播放", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+//        GiraffePlayer.play(getBaseContext(), videoInfo);
+        binding.videoView.videoInfo(videoInfo);
+        binding.videoView.getPlayer().start();
     }
 
     @Override
